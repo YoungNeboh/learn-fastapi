d9ef591b698a5d56402b43ef38cd4396a75ef027
Young Neboh <davidsonneb@gmail.com>
2026-01-22 11:39:12 +0100

    Changes

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def index():
 @app.get("/about")
 def about():
     return {"data": "This is the about page."}
+
+@app.get("/about/{id}")
+def show(id: int):
+    return {"data": "Let's work with id: {id}".format(id=id),
+            "data2": id}
