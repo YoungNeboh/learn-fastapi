59ce3669ba4fae8a1c6fbe68c8c5720b620eb56c
Young Neboh <davidsonneb@gmail.com>
2026-03-02 15:17:42 +0100

    Created Models directory and segmented models appropriately

--- models/organization.py
+++ models/organization.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from uuid import UUID, uuid4
 from sqlmodel import SQLModel, Field, Relationship
-from.user import User, UserOrgLink
+from.user import User, Memberships
 
 
 class Organization(SQLModel, table=True):
@@ -10,4 +10,4 @@ class Organization(SQLModel, table=True):
     id: Optional[UUID] = Field(default_factory=uuid4, primary_key=True)
     name: str
     # I don't need "back_populates' here because I only need to view all the users associated 
-    users: list["User"] = Relationship(link_model=UserOrgLink)
+    users: list["User"] = Relationship(back_populates="organizations", link_model=Memberships)
--- models/user.py
+++ models/user.py
@@ -4,11 +4,20 @@ from sqlmodel import SQLModel, Field, Relationship
 from .base import MultiTenantBase
 from .shopping import CartItems
 from .constants import UserRole
-
+from .organization import Organization
 
 class UserOrgLink(SQLModel, table=True):
     user_id: UUID = Field(foreign_key="user.id", primary_key=True)
     org_id: UUID = Field(foreign_key="organization.id", primary_key=True)
+
+class Memberships(MultiTenantBase, table=True):
+    __tablename__ = "memberships"
+
+    user_id: UUID = Field(foreign_key="user.id", primary_key=True, index=True)
+    org_id: UUID = Field(foreign_key="organization.id", primary_key=True, index=True)
+    role: UserRole = Field(default=UserRole.CUSTOMER)
+    
+    user: "User" = Relationship(back_populates="memberships")
 
 class User(SQLModel, table=True):
     __tablename__ = "user"
@@ -16,14 +25,8 @@ class User(SQLModel, table=True):
     id: Optional[UUID] = Field(default_factory=uuid4, primary_key=True)
     name: str
 
-    memberships: list["Memberships"] = Relationship(back_populates="user", link_model=UserOrgLink)
+    memberships: list["Memberships"] = Relationship(back_populates="user")
     cart_items: list["CartItems"] = Relationship(back_populates="user")
+    organizations: list["Organization"] = Relationship(back_populates="users", link_model=Memberships)
 
-class Memberships(MultiTenantBase, table=True):
-    __tablename__ = "memberships"
-
-    user_id: UUID = Field(foreign_key="user.id", index=True)
-    role: UserRole = Field(default=UserRole.CUSTOMER)
-    
-    user: "User" = Relationship(back_populates="memberships")
 
